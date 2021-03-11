445587ebf23567aee7c2b80c374554f357a21657
Ole Algoritme <olealgoritme@gmail.com>
2021-03-11 10:30:51 +0000

    added setup for ubuntu

---
+++ setup.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+
+# UBUNTU SETUP
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+sudo apt-key fingerprint 0EBFCD88
+sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
+sudo apt update
+sudo apt install docker-ce docker-compose -y
+sudo usermod -aG docker `whoami`
